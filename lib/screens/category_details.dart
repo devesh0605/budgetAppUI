38e38b0f5663c4806cb2c52be09cde283af3fc93
devesh0605 <dsaxena_be18@thapar.edu>
2021-05-30 18:35:23 +0530

    radial progress bar

--- lib/screens/category_details.dart
+++ lib/screens/category_details.dart
@@ -1,4 +1,8 @@
+import 'package:budget_app_ui/helpers/color_helper.dart';
 import 'package:budget_app_ui/models/category_model.dart';
+import 'package:budget_app_ui/models/expense_model.dart';
+import 'package:budget_app_ui/widgets/radial_painter.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CategoryDetailsScreen extends StatefulWidget {
@@ -13,6 +17,17 @@ class CategoryDetailsScreen extends StatefulWidget {
 class _CategoryDetailsScreenState extends State<CategoryDetailsScreen> {
   @override
   Widget build(BuildContext context) {
+    double totalAmountSpent = 0.0;
+
+    widget.currentCategory.expenses.forEach((Expense expense) {
+      totalAmountSpent += expense.cost;
+    });
+
+    final double amountLeft =
+        widget.currentCategory.maxAmount - totalAmountSpent;
+
+    final double percent = amountLeft / widget.currentCategory.maxAmount;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.currentCategory.name),
@@ -28,34 +43,50 @@ class _CategoryDetailsScreenState extends State<CategoryDetailsScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            MediaQuery.removePadding(
-              context: context,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Card(
-                  elevation: 10,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Card(
+                color: Colors.green[200],
+                margin: EdgeInsets.all(10),
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Container(
+                  child: CustomPaint(
+                    foregroundPainter: RadialPainter(
+                      bgColor: Colors.grey[200],
+                      lineColor: getColor(context, percent),
+                      percent: percent,
+                      width: 15.0,
+                    ),
+                    child: Center(
+                      child: Text(
+                        '\$${amountLeft.toStringAsFixed(2)}/\$${widget.currentCategory.maxAmount.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
                   ),
-                  child: Container(
-                    //margin: EdgeInsets.all(20),
-                    //padding: EdgeInsets.all(20),
-                    height: 250,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            offset: Offset(0, 2),
-                            blurRadius: 6.0,
-                          )
-                        ]),
-                  ),
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(20),
+                  height: 250,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                            // color: Colors.black12,
+                            // offset: Offset(0, 2),
+                            // blurRadius: 6.0,
+                            )
+                      ]),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
